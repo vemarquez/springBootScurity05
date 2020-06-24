6b50851a0761c5b628a0423591ead26a7ee2489e
Victor E. Márquez Flores <victor.marquez.flores@gmail.com>
2020-06-23 20:18:36 -0500

    cambios_23062020

--- src/main/java/com/mvit/security/controller/AppController.java
+++ src/main/java/com/mvit/security/controller/AppController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -75,5 +76,10 @@ public class AppController {
 		logger.info("Se borra el registro id :: " + id );
 		service.deleteCntPartidaById(id);		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/403")
+	public String error403() {
+		return "403";
 	}
 }
